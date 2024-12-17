b1bfd3ad5987b810218081f39fdea6e875306754
Huaiying Luo <hl2446@cornell.edu>
2024-12-16 22:11:17 -0500

    initial commit

---
+++ backend/README.md
@@ -0,0 +1,18 @@
+# Moviest
+
+## Run the application
+
+To run the application, run the following command:
+```
+./mvnw spring-boot:run
+```
+
+## System Design
+
+Java Spring Boot backend with MongoDB database
+
+## API explained 
+get all movies
+get a single movie
+post a review
+
---
+++ backend/src/main/java/com/hyluo/moviest/MoviestApplication.java
@@ -0,0 +1,22 @@
+package com.hyluo.moviest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MoviestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestApplication.class, args);
+	}
+
+
+
+
+
+}
+
+
